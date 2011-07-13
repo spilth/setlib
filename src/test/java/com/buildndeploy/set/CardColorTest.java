c735cd97ed660a1f3b101d375832e3c33a09d4ba
Brian Kelly <polymonic@gmail.com>
2011-07-13 17:04:51 -0400

    Testing all combinations of Card Colors

--- src/test/java/com/buildndeploy/set/CardColorTest.java
+++ src/test/java/com/buildndeploy/set/CardColorTest.java
@@ -9,11 +9,18 @@ public class CardColorTest {
 	@Test
 	public void similarColors() {
 		assertEquals(CardColor.RED, CardColor.findThirdCardColor(CardColor.RED, CardColor.RED));
+		assertEquals(CardColor.GREEN, CardColor.findThirdCardColor(CardColor.GREEN, CardColor.GREEN));
+		assertEquals(CardColor.PURPLE, CardColor.findThirdCardColor(CardColor.PURPLE, CardColor.PURPLE));
 	}
 
 	@Test
 	public void disimilarColors() {
 		assertEquals(CardColor.GREEN, CardColor.findThirdCardColor(CardColor.RED, CardColor.PURPLE));
+		assertEquals(CardColor.GREEN, CardColor.findThirdCardColor(CardColor.PURPLE, CardColor.RED));
+		assertEquals(CardColor.PURPLE, CardColor.findThirdCardColor(CardColor.GREEN, CardColor.RED));
+		assertEquals(CardColor.PURPLE, CardColor.findThirdCardColor(CardColor.RED, CardColor.GREEN));
+		assertEquals(CardColor.RED, CardColor.findThirdCardColor(CardColor.GREEN, CardColor.PURPLE));
+		assertEquals(CardColor.RED, CardColor.findThirdCardColor(CardColor.PURPLE, CardColor.GREEN));
 	}
 
 }
