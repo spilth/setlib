23107dbb95ed6dd1c06de4f9f2c8fd61b2336445
Brian Kelly <polymonic@gmail.com>
2011-07-17 21:55:55 -0400

    Implemented Solver.findSets. Returns a List of integer arrays (indexes of cards that make up a Set)

--- src/main/java/com/buildndeploy/set/Solver.java
+++ src/main/java/com/buildndeploy/set/Solver.java
@@ -1,5 +1,8 @@
 package com.buildndeploy.set;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solver {
 
 	public static boolean isSet(Card a, Card b, Card c) {
@@ -9,6 +12,30 @@ public class Solver {
 				SymbolShading.isSet(a.getShading(), b.getShading(), c.getShading()) &&
 				SymbolShape.isSet(a.getShape(), b.getShape(), c.getShape())
 		);
+
+	}
+
+	public static List<int[]> findSets(List<Card> cardList) {
+		List<int[]> sets = new ArrayList<int[]>();
+		
+		Object[] cards = cardList.toArray();
+		
+		Card first,second,third;
+		
+		for (int i = 0; i <cards.length; i++) {
+			for (int j = i+1; j < cards.length; j++) {
+				for (int k = j+1; k < cards.length ; k++) {
+					first = (Card) cards[i];
+					second = (Card) cards[j];
+					third = (Card) cards[k];
+					if (isSet(first, second, third)) {
+						sets.add(new int[] {i,j,k});
+					}
+				}
+			}
+		}
+
+		return sets;
 
 	}
 
--- src/test/java/com/buildndeploy/set/SolverTest.java
+++ src/test/java/com/buildndeploy/set/SolverTest.java
@@ -2,6 +2,9 @@ package com.buildndeploy.set;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class SolverTest {
@@ -14,10 +17,31 @@ public class SolverTest {
 		assertTrue(Solver.isSet(a, b, c));
 	}
 
+	@Test
 	public void complexSet() {
 		Card a = new Card(SymbolCount.ONE, SymbolColor.RED, SymbolShape.CAPSULE, SymbolShading.SOLID);
 		Card b = new Card(SymbolCount.TWO, SymbolColor.GREEN, SymbolShape.DIAMOND, SymbolShading.SHADED);
 		Card c = new Card(SymbolCount.THREE, SymbolColor.PURPLE, SymbolShape.SQUIGGLE, SymbolShading.CLEAR);
 		assertTrue(Solver.isSet(a, b, c));
+	}
+
+	@Test
+	public void findSet() {
+		Card a = new Card(SymbolCount.ONE, SymbolColor.RED, SymbolShape.CAPSULE, SymbolShading.SOLID);
+		Card b = new Card(SymbolCount.TWO, SymbolColor.RED, SymbolShape.CAPSULE, SymbolShading.SOLID);
+		Card c = new Card(SymbolCount.TWO, SymbolColor.GREEN, SymbolShape.CAPSULE, SymbolShading.SOLID);
+		Card d = new Card(SymbolCount.THREE, SymbolColor.RED, SymbolShape.CAPSULE, SymbolShading.SOLID);
+		Card e = new Card(SymbolCount.TWO, SymbolColor.PURPLE, SymbolShape.CAPSULE, SymbolShading.SOLID);
+
+		List<Card> cards = new ArrayList<Card>();
+		cards.add(a);
+		cards.add(b);
+		cards.add(c);
+		cards.add(d);
+		cards.add(e);
+		
+		assertEquals(2, Solver.findSets(cards).size());
+		assertArrayEquals(new int[] {0,1,3}, Solver.findSets(cards).get(0));
+		assertArrayEquals(new int[] {1,2,4}, Solver.findSets(cards).get(1));
 	}
 }
